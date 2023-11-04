cbfcb51c2a811f669be42e14393383de412852d8
“sasaoka” <133482338+seiji-sa@users.noreply.github.com>
2023-11-04 16:06:11 +0900

    tb更新

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
+
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
-gulp.task('default', ()=> {
-    return(
+gulp.task('default', () => {
+    return (
         gulp.src('sass/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('css'))
-    )
+            .pipe(sass())
+            .pipe(gulp.dest('css'))
+    );
 });
-
