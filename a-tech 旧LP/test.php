629354bdf747a2cf0fc815fb1eed1f1e97d21852
“sasaoka” <133482338+seiji-sa@users.noreply.github.com>
2023-12-13 10:17:36 +0900

    gulp更新

--- a-tech 旧LP/test.php
+++
@@ -1,2 +0,0 @@
-<?php echo __FILE__;
-echo file_exists('/home/users/0/sub.jp-s-tech/web/test/ /.htpasswd') ? 'OK' : 'NG';
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,34 @@
 
-const gulp = require('gulp');
+const {src, watch, dest, series} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssSorter = require("css-declaration-sorter");
+const mmq = require("gulp-merge-media-queries");
 
-gulp.task('default', () => {
-    return (
-        gulp.src('sass/**/*.scss')
-            .pipe(sass())
-            .pipe(gulp.dest('css'))
-    );
-});
+
+const sassBuild = () =>{
+    return src('src/sass/*.scss')
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(dest('dist/css'))
+    .pipe(postcss([autoprefixer(),cssSorter({order:'smacss'})]))
+    .pipe(mmq())
+}
+
+const srcPath = {
+    html: 'src/index.html',
+    css: 'src/sass/*.scss'
+}
+
+const distPath = {
+    css: 'dist/css/style.css'
+}
+
+const watchFile = () =>{
+    watch(srcPath.css, sassBuild)
+}
+
+exports.default = series(sassBuild,watchFile);
+exports.build = sassBuild;
+
+
